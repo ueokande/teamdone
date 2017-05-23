6fe88b41b00a65af7e15d13858ced31fd7dad452
Shin'ya Ueoka <ueokande@i-beam.org>
2017-05-23 21:51:52 +0900

    Add members query method

--- vendor/app/model/member.go
+++ vendor/app/model/member.go
@@ -16,3 +16,45 @@ func MemberCreate(oid, uid int64) error {
 		oid, uid)
 	return err
 }
+
+func MembersByUserId(uid int64) ([]int64, error) {
+	rows, err := database.SQL.Query(
+		"SELECT org_id FROM member WHERE user_id = ?",
+		uid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	oids := make([]int64, 0)
+	for rows.Next() {
+		var o int64
+		err := rows.Scan(&o)
+		if err != nil {
+			return nil, err
+		}
+		oids = append(oids, o)
+	}
+	return oids, nil
+}
+
+func MembersByOrgId(oid int64) ([]int64, error) {
+	rows, err := database.SQL.Query(
+		"SELECT user_id FROM member WHERE org_id = ?",
+		oid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	uids := make([]int64, 0)
+	for rows.Next() {
+		var u int64
+		err := rows.Scan(&u)
+		if err != nil {
+			return nil, err
+		}
+		uids = append(uids, u)
+	}
+	return uids, nil
+}
--- vendor/app/model/member_test.go
+++ vendor/app/model/member_test.go
@@ -3,6 +3,7 @@ package model
 import (
 	"app/shared"
 	"database/sql"
+	"reflect"
 	"testing"
 )
 
@@ -15,10 +16,11 @@ func setup() ([]int64, []int64, error) {
 	users := []string{
 		"alice",
 		"bob",
+		"carol",
 	}
 	members := map[int][]int{
-		0: {0},
-		1: {1},
+		0: {0, 2},
+		1: {1, 2},
 	}
 
 	oid := make([]int64, len(orgs), len(orgs))
@@ -57,7 +59,15 @@ func TestMemberExists(t *testing.T) {
 	if err != nil {
 		t.Fatal("Unexpected error:", err)
 	}
+	err = MemberExists(oid[0], uid[2])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
 	err = MemberExists(oid[1], uid[1])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	err = MemberExists(oid[1], uid[2])
 	if err != nil {
 		t.Fatal("Unexpected error:", err)
 	}
@@ -70,4 +80,58 @@ func TestMemberExists(t *testing.T) {
 		t.Fatal("Unexpected error:", err)
 	}
 
+}
+
+func TestMembersByOrgId(t *testing.T) {
+	oid, uid, err := setup()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	member, err := MembersByOrgId(oid[0])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if !reflect.DeepEqual(member, []int64{uid[0], uid[2]}) {
+		t.Fatal("Unexpected member:", member)
+	}
+
+	member, err = MembersByOrgId(oid[1])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if !reflect.DeepEqual(member, []int64{uid[1], uid[2]}) {
+		t.Fatal("Unexpected member:", member)
+	}
+}
+
+func TestMembersByUserId(t *testing.T) {
+	oid, uid, err := setup()
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+
+	member, err := MembersByUserId(uid[0])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if !reflect.DeepEqual(member, []int64{oid[0]}) {
+		t.Fatal("Unexpected member:", member)
+	}
+
+	member, err = MembersByUserId(uid[1])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if !reflect.DeepEqual(member, []int64{oid[1]}) {
+		t.Fatal("Unexpected member:", member)
+	}
+
+	member, err = MembersByUserId(uid[2])
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	if !reflect.DeepEqual(member, []int64{oid[0], oid[1]}) {
+		t.Fatal("Unexpected member:", member)
+	}
 }
