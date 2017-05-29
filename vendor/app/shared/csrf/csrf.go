7327fa833b5e7e05259efcf301668372826efc36
Shin'ya Ueoka <ueokande@i-beam.org>
2017-05-29 22:14:33 +0900

    add csrf handler

---
+++ vendor/app/shared/csrf/csrf.go
@@ -0,0 +1,67 @@
+package csrf
+
+import (
+	"crypto/subtle"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type CSRFHandler struct {
+	HeaderName   string
+	CookieName   string
+	TokenLength  int
+	CookieMaxAge int
+}
+
+var DefaultCSRFHandler = CSRFHandler{
+	TokenLength:  32,
+	HeaderName:   "X-Request-Token",
+	CookieName:   "_request_token",
+	CookieMaxAge: 86400,
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+)
+
+func randomToken(length int) string {
+	b := make([]byte, length)
+	for i := 0; i < length; {
+		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i++
+		}
+	}
+	return string(b)
+}
+
+func (h CSRFHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(h.CookieName)
+	var token string
+	if err != nil {
+		token = randomToken(h.TokenLength)
+	} else {
+		token = cookie.Value
+	}
+
+	switch r.Method {
+	case http.MethodGet, http.MethodHead, http.MethodOptions, http.MethodTrace:
+	default:
+		reqtoken := r.Header.Get(h.HeaderName)
+		if subtle.ConstantTimeCompare([]byte(token), []byte(reqtoken)) != 1 {
+			http.Error(w, "invalid request token", http.StatusForbidden)
+			return
+		}
+	}
+
+	cookie = &http.Cookie{
+		Name:    h.CookieName,
+		Value:   token,
+		Expires: time.Now().Add(time.Duration(h.CookieMaxAge) * time.Second),
+	}
+	http.SetCookie(w, cookie)
+	w.Header().Set("Vary", "Cookie")
+}
---
+++ vendor/app/shared/csrf/csrf_test.go
@@ -0,0 +1,62 @@
+package csrf
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo"
+)
+
+func cookieByName(cookies []*http.Cookie, name string) *http.Cookie {
+	for _, c := range cookies {
+		if c.Name == name {
+			return c
+		}
+	}
+	return nil
+}
+
+func TestServeHTTP_Get(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+
+	DefaultCSRFHandler.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	cookie := cookieByName(res.Cookies(), "_request_token")
+	if cookie == nil {
+		t.Fatal("Unexpected response:", *res)
+	}
+	if len(cookie.Value) != 32 {
+		t.Fatal("Unexpected CSRF token:", cookie.Value)
+	}
+}
+
+func TestServeHTTP_ValidPost(t *testing.T) {
+	cases := []struct {
+		token  string
+		set    bool
+		status int
+	}{
+		{"secret-csrf-token", true, http.StatusOK},
+		{"", true, http.StatusForbidden},
+		{"", false, http.StatusForbidden},
+	}
+
+	for _, c := range cases {
+		req := httptest.NewRequest(echo.POST, "/", nil)
+		req.Header.Add("Cookie", "_request_token=secret-csrf-token")
+		if c.set {
+			req.Header.Set("X-Request-Token", c.token)
+		}
+		rec := httptest.NewRecorder()
+
+		DefaultCSRFHandler.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		if res.StatusCode != c.status {
+			t.Fatal("Unexpected response:", *res)
+		}
+	}
+}
